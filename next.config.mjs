80ae4f765c9e5531a63da528cbcaab06d7233aa6
Ismailhajjix <ismailhajji.official@gmail.com>
2025-01-31 21:26:28 +0100

    feat: configure build caching

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,20 @@ const nextConfig = {
   },
   trailingSlash: true,
   reactStrictMode: true,
-  poweredByHeader: false
+  poweredByHeader: false,
+  experimental: {
+    turbotrace: {
+      logLevel: "error"
+    }
+  },
+  distDir: '.next',
+  generateBuildId: async () => {
+    return 'build-' + Date.now()
+  },
+  onDemandEntries: {
+    maxInactiveAge: 60 * 60 * 1000,
+    pagesBufferLength: 2,
+  }
 }
 
 export default nextConfig
